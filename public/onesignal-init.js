01964e710e4e714632cbda2270a8d34396ce7b45
Fe Dev.XYZ <fe.dev.xyz@gmail.com>
2025-10-31 18:19:07 +0100

    Delete public/onesignal-init.js

--- public/onesignal-init.js
+++
@@ -1,9 +0,0 @@
-window.OneSignalDeferred = window.OneSignalDeferred || [];
-   OneSignalDeferred.push(async function(OneSignal) {
-     await OneSignal.init({
-       appId: "c8b1aa7c-66f2-4778-9749-1a11d4c2a670", // <-- Metti qui il tuo App ID
-       serviceWorkerParam: { scope: '/aiutatemi/' },
-       serviceWorkerPath: '/aiutatemi/public/OneSignalSDKWorker.js',
-       path: '/aiutatemi/',
-     });
-   });
